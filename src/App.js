8275e6630427b4efb3f9b0a93aad2ce40d5bd18b
khaledsallem <70590665+khaledsallem@users.noreply.github.com>
2021-02-20 10:39:50 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react'
-import Movies from './Components/Movies';
 import Header from './Components/Header';
+import Accueil from './Components/Accueil';
+import Movies from './Components/Movies';
+import Contact from './Components/Contact';
+import Blog from './Components/Blog';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
-class App extends Component {
 
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <div className='container'>
-          <Movies />
-        </div>
-      </div>
-    );
-  }
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Router>
+        <Route path="/" exact component={Accueil} />
+        <Route path="/Movies" exact component={Movies} />
+        <Route path="/Contact" exact component={Contact} />
+        <Route path="/Blog" exact component={Blog} />
+      </Router>
+    </div>
+  );
 }
+
 
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,39 @@
-import React, {Component} from 'react'
-class Header extends Component {
+import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 
-    render() {
-        return (
-            <div className="header">
-                <nav className="navbar navbar-light bg-dark justify-content-around">
-                    <a className="navbar-brand">Movie App</a>
-                    <form className="form-inline">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search Movie" aria-label="Search"></input>
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </nav>
-            </div>
-        )
-    }
+function Header() {
+
+    return (
+        <div className="header">
+            <ul style={{ display: 'flex', listStyle: 'none', justifyContent: 'space-between', margin: '10px' }}>
+                <Link to='/'>
+                    <li>Accueil</li>
+
+                </Link>
+
+                <Link to='/Movies'>
+                    <li>Movies</li>
+                </Link>
+
+                <Link to='/Contact'>
+                    <li>Contact</li>
+                </Link>
+
+                <Link to='/Blog'>
+                    <li>Blog</li>
+                </Link>
+
+                <li> <form className="form-inline">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search Movie" aria-label="Search"></input>
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form></li>
+            </ul>
+
+        </div>
+    )
 }
+
+
 
 
 
